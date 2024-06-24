d799d458d8ed4fdc41be61bf854dea747adf5705
Tanay Chakraborty <tanaychakraborty.55@gmail.com>
2024-06-24 09:40:15 +0530

    19th

---
+++ DSA--JAVA/src/Trees/Questions/Level_order_Traversal.java
@@ -0,0 +1,76 @@
+package Trees.Questions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Level_order_Traversal {
+
+
+
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+        }
+    }
+
+
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int levelSize  = queue.size();
+
+            List<Integer> currentlevel = new ArrayList<>(levelSize);
+            for(int i =0;i<levelSize;i++){
+                TreeNode currentNode = queue.poll();
+                currentlevel.add(currentNode.val);
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentlevel);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        Level_order_Traversal traversal = new Level_order_Traversal();
+        List<List<Integer>> result = traversal.levelOrder(root);
+
+        for (List<Integer> level : result) {
+            System.out.println(level);
+        }
+    }
+}
